a603778b8a9a585dc76c16b90ade71bcebd6c45d
unknown <ghareeb_shehata@outlook.com>
2020-09-29 14:40:56 +0200

    Updates

---
+++ precache-manifest.5d68a4c653b5adccb2944a708c2084ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dfc3a1486f9938a9d801d9c4740bd9d",
+    "url": "/React-Todo-list/index.html"
+  },
+  {
+    "revision": "5879e347ed55830fa348",
+    "url": "/React-Todo-list/static/css/main.437d068b.chunk.css"
+  },
+  {
+    "revision": "bb2a207390460618e7e0",
+    "url": "/React-Todo-list/static/js/2.816442d3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Todo-list/static/js/2.816442d3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5879e347ed55830fa348",
+    "url": "/React-Todo-list/static/js/main.aa82b080.chunk.js"
+  },
+  {
+    "revision": "2a01963ed7f21848f538",
+    "url": "/React-Todo-list/static/js/runtime-main.3623f922.js"
+  }
+]);
